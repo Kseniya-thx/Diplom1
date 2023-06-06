12fef8650279fc8860846ffba3940612ac4ddadd
Kseniya-thx <kseniya.marchenkova.km@gmail.com>
2023-06-06 13:02:16 +0300

    workk

---
+++ Diplom1/Controllers/DepartController.cs
@@ -0,0 +1,42 @@
+﻿using Diplom1.Components;
+using Diplom1.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Diplom1.Controllers
+{
+    public class DepartController : Controller
+    {
+        public string SerchTerm { get; set; }
+
+        // GET: Person
+        private readonly MySqlConnection _connection = ConnectionManager.GetConnection();
+        // GET: Depart
+        public ActionResult Index()
+        {
+            var depart = new List<Depart>();
+            var command = new MySqlCommand("select * from departments", _connection);
+            using (var reader = command.ExecuteReader())
+
+            {
+                while (reader.Read())
+                {
+                    var departt = new Depart
+                    {
+                        Id = Convert.ToInt32(reader["id"]),
+                        Name_Depart = reader["name_depart"].ToString(),
+                       
+                    };
+
+                    depart.Add(departt);
+                }
+            }
+
+            return View(depart);
+        }
+    }
+}
---
+++ Diplom1/Models/Depart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Diplom1.Models
+{
+    public class Depart
+    {
+        public int Id { get; set; }
+
+        public string Name_Depart { get; set; }
+
+    }
+}
